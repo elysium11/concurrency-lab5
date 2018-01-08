56dab7221e39b5f2aa018a9fe3a3ce51deaa4dd6
Ilnur Khafizov <ilnurkhafizoff@yandex.ru>
2018-01-08 03:22:47 +0300

    Fix log names in info msg

--- conf/put-logs-to-hdfs.sh
+++ conf/put-logs-to-hdfs.sh
@@ -3,4 +3,4 @@ hdfs dfs -mkdir /nasa_logs
 hdfs dfs -put NASA_access_log_Aug95 /nasa_logs
 hdfs dfs -put NASA_access_log_Jul95 /nasa_logs
 
-echo "access_log_Aug95 and access_log_Jul95 were put under '/nasa_logs' hdfs dir"
+echo "NASA_access_log_Aug95 and NASA_access_log_Jul95 were put under '/nasa_logs' hdfs dir"
